822f263cb9556518b54a244e27eac1cf18bf510f
Daniel Baumann <danieljames3@gmail.com>
2011-09-19 17:30:19 -0600

    Added pearson metric with test. Switched to raising an exception if vector lengths don't agree.

--- project04/similarity_metrics/proximity.py
+++ project04/similarity_metrics/proximity.py
@@ -5,42 +5,76 @@ Values closer to 1 represent a better match."""
 
 def euclidean(vector1, vector2):
 	"""Traditional linear distance between the endpoints of two vectors of numeric values.
-	If one vector is longer than another, the extra values of the longer are ignored."""
+	
+	Raises ValueError if one vector is longer than another."""
 
-	shortest_length = min(len(vector1), len(vector2))
+	if(len(vector1) != len(vector2)): raise ValueError
 
-	sum_of_squares = sum([pow(vector1[i] - vector2[i], 2) for i in range(shortest_length)])
+	sum_of_squares = sum([pow(vector1[i] - vector2[i], 2) for i in range(len(vector1))])
 
 	#greater distance shold mean lower similarity
 	return 1/(1+sqrt(sum_of_squares))
 
 def smc(vector1, vector2):
 	"""Simple measure of similarity for vectors of binary values.
-	If one vector is longer than another, the extra values of the longer are ignored.
 
-	Raises ValueError for non-binary input vector."""
+	Raises ValueError for non-binary input vector.
+	Raises ValueError if one vector is longer than another."""
 
 	union = list(set(vector1) | set(vector2))
 	if(max(union) > 1 or min(union) < 0): raise ValueError
 
-	shortest_length = min(len(vector1), len(vector2))
+	if(len(vector1) != len(vector2)): raise ValueError
+
 	match0,match1 = 0,0
 
-	for i in range(shortest_length):
+	for i in range(len(vector1)):
 		if(vector1[i]==0 and vector2[i]==0): match0 += 1
 		if(vector1[i]==1 and vector2[i]==1): match1 += 1
 		
-	return float(match0 + match1)/shortest_length
+	return float(match0 + match1)/len(vector1)
 
 def jaccard(vector1, vector2):
+	"""Measure of similarity for vectors of binary values. Non-presence matches are
+	disregarded.
+
+	Raises ValueError for non-binary input vector.
+	Raises ValueError if one vector is longer than another."""
 	union = list(set(vector1) | set(vector2))
 	if(max(union) > 1 or min(union) < 0): raise ValueError
 
-	shortest_length = min(len(vector1), len(vector2))
+	if(len(vector1) != len(vector2)): raise ValueError
+
 	match0,match1 = 0,0
 
-	for i in range(shortest_length):
+	for i in range(len(vector1)):
 		if(vector1[i]==0 and vector2[i]==0): match0 += 1
 		if(vector1[i]==1 and vector2[i]==1): match1 += 1
 		
-	return float(match1)/(shortest_length-match0)
+	return float(match1)/(len(vector1)-match0)
+
+def pearson(vector1, vector2):
+	"""Measure of similarity for vectors of numeric values.
+
+	Return values in [-1,0) indicate dissimilarity.
+
+	Raises ValueError if one vector is longer than another."""
+
+	if(len(vector1) != len(vector2)): raise ValueError
+
+	numerator = __covariance(vector1, vector2)
+	denominator = sqrt(__covariance(vector1, vector1))*sqrt(__covariance(vector2, vector2))
+
+	return numerator/denominator
+
+def __covariance(vector1, vector2):
+
+	if(len(vector1) != len(vector2)): raise ValueError
+
+	vector1_avg = float(sum(vector1))/len(vector1)
+	vector2_avg = float(sum(vector2))/len(vector2)
+
+	cov_sum =  sum([(vector1[i]-vector1_avg)*(vector2[i]-vector2_avg)
+				for i in range(len(vector1))])
+
+	return cov_sum/(len(vector1) - 1)
--- project04/similarity_metrics/proximity_test.py
+++ project04/similarity_metrics/proximity_test.py
@@ -6,11 +6,11 @@ from math import sqrt
 class TestSimilarityMetrics(unittest.TestCase):
 
 	def test_euclidean(self):
-		vector1 = vector2 = [1,2,3]
+		vector1 = vector2 = [1, 2, 3]
 		expected = 1
 		self.assertEqual(expected, proximity.euclidean(vector1, vector2))
 
-		vector2 = [4,5,6]
+		vector2 = [4, 5, 6]
 		expected = 1/(1+sqrt(27))
 		self.assertEqual(expected, proximity.euclidean(vector1, vector2))
 
@@ -55,6 +55,22 @@ class TestSimilarityMetrics(unittest.TestCase):
 		vector2 = [0, 0, 1, 1, 1]
 		expected = 2/4.0
 		self.assertEqual(expected, proximity.jaccard(vector1, vector2))
+
+	def test_pearson(self):
+		vector1 = [1, 2, 3, 4]
+
+		vector2 = vector1
+		expected = 1
+		self.assertAlmostEqual(expected, proximity.pearson(vector1, vector2))
+
+		vector2 = [4, 3, 2, 1]
+		expected = -1
+		self.assertAlmostEqual(expected, proximity.pearson(vector1, vector2))
+
+		vector2 = [5, 9, 11, 2]
+		expected = -.224179415
+		self.assertAlmostEqual(expected, proximity.pearson(vector1, vector2))
+
 
 	
 
