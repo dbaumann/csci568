adbae19a21bfc04e40dd5d59fbb0f3d6578ecfbc
Daniel Baumann <danieljames3@gmail.com>
2011-09-19 10:19:42 -0600

    Added smc metric with test.

--- project04/similarity_metrics/proximity.py
+++ project04/similarity_metrics/proximity.py
@@ -4,9 +4,31 @@ from math import sqrt
 Values closer to 1 represent a better match."""
 
 def euclidean(vector1, vector2):
-	"""Traditional linear distance between the endpoints of two vectors."""
+	"""Traditional linear distance between the endpoints of two vectors of numeric values.
+	If one vector is longer than another, the extra values of the longer are ignored."""
 
-	sum_of_squares = sum([pow(vector1[i] - vector2[i], 2) for i in range(len(vector1))])
+	shortest_length = min(len(vector1), len(vector2))
+
+	sum_of_squares = sum([pow(vector1[i] - vector2[i], 2) for i in range(shortest_length)])
 
 	#greater distance shold mean lower similarity
 	return 1/(1+sqrt(sum_of_squares))
+
+def smc(vector1, vector2):
+	"""Simple measure of similarity for vectors of binary values.
+	If one vector is longer than another, the extra values of the longer are ignored.
+
+	Raises ValueError for non-binary input vector."""
+
+	union = list(set(vector1) | set(vector2))
+	if(max(union) > 1 or min(union) < 0): raise ValueError
+
+	shortest_length = min(len(vector1), len(vector2))
+
+	match0,match1 = 0,0
+
+	for i in range(shortest_length):
+		if(vector1[i]==0 and vector2[i]==0): match0 += 1
+		if(vector1[i]==1 and vector2[i]==1): match1 += 1
+		
+	return (match0 + match1)/shortest_length
--- project04/similarity_metrics/proximity_test.py
+++ project04/similarity_metrics/proximity_test.py
@@ -14,5 +14,26 @@ class TestSimilarityMetrics(unittest.TestCase):
 		expected = 1/(1+sqrt(27))
 		self.assertEqual(expected, proximity.euclidean(vector1, vector2))
 
+	def test_smc(self):
+		#should only accept vectors of binary values
+		vector1 = [1, 0, 0, 1, 1]
+		vector2 = [1, 0, 0, 1, -1]
+		self.assertRaises(ValueError, proximity.smc, vector1, vector2)
+		vector2 = [1, 0, 0, 1, 2]
+		self.assertRaises(ValueError, proximity.smc, vector1, vector2)
+
+		vector2 = vector1
+		expected = 1
+		self.assertEqual(expected, proximity.smc(vector1, vector2))
+
+		vector2 = [0, 1, 1, 0, 0]
+		expected = 0
+		self.assertEqual(expected, proximity.smc(vector1, vector2))
+
+		vector2 = [1, 1, 1, 1, 1]
+		expected = 3/5
+		self.assertEqual(expected, proximity.smc(vector1, vector2))
+	
+
 suite = unittest.TestLoader().loadTestsFromTestCase(TestSimilarityMetrics)
 unittest.TextTestRunner(verbosity=2).run(suite)
