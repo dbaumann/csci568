7099ccb824d2d91132c36add487fc49805b63222
Daniel Baumann <danieljames3@gmail.com>
2011-09-27 15:15:03 -0600

    Tweaked k-means algorithm to produce more consistent results for the iris data set. Improved detail of generated output.
    Updated README with discussion of algorithm customizations and
    comparison of results to WEKA output.

--- project05/clusters.py
+++ project05/clusters.py
@@ -4,22 +4,13 @@ import data
 import distance
 import prototype
 
-import pprint
-pp = pprint.PrettyPrinter(indent=2)
-
-
-
 def kmeans(data, k=4, distance=distance.euclidean, prototype=prototype.rand):
 	attributes = range(len(data[0]))
 
 	centroids = prototype(data, k)
 
-	print "initial centroids:"
-	print centroids
-
 	clusters_last = None
-	for t in range(100):
-		print 'Iteration %d' % t
+	for t in range(1, 101):
 		clusters = [[] for i in range(k)]
 
 		#assign each object to the closest centroid
@@ -52,34 +43,84 @@ def kmeans(data, k=4, distance=distance.euclidean, prototype=prototype.rand):
 				
 				centroids[i] = attr_averages
 	
+	print "kmeans finished after %d iterations" % t
+	
 	return clusters, centroids
 
 def cluster_sse(data, cluster, centroid, distance=distance.euclidean):
 	return sum([pow(distance(data[object_num], centroid), 2) for object_num in cluster])
 
-iris_data, iris_keys = data.iris()
+def total_sse(data, clusters, centroids, distance=distance.euclidean):
+	return sum([cluster_sse(data, clusters[i], centroids[i], distance)
+					for i in range(len(clusters))])
+
+def summary_stats(data, keys):
+	attributes = range(len(data[0]))
+
+	attr_ranges = [(min([obj[i] for obj in data]), max([obj[i] for obj in data]))
+				for i in attributes]
+
+	attr_averages = [sum([obj[i] for obj in data])/len(data) for i in attributes]
 
 
-#for euclidean
-for i in range(2, 5):
-	print "kmeans result clusters based on euclidean distance for k = %d:" % i
+	attr_strings = []
 
-	clusters, centroids = kmeans(iris_data, k=i, distance=distance.euclidean,
-							prototype=prototype.avg)
+	for i in attributes:
+		attr_strings.append("%.3f-->%.3f (%.3f)" % \
+							(attr_ranges[i][0],attr_ranges[i][1], attr_averages[i]))
 
+	return "[%s]" % ', '.join(attr_strings)
+
+
+def print_clustering(data, clusters, centroids, distance=distance.euclidean):
 	sse_values = []
 
 	for j in range(i):
 
-		sse_values.append(cluster_sse(iris_data, clusters[j], centroids[j],
-							distance=distance.euclidean)) 
+		sse_values.append(cluster_sse(iris_data, clusters[j], centroids[j], distance)) 
 
 		print "%d {size: %d, sse: %f, centroid:[%s]}" % \
 			(j,
 			len(clusters[j]),
 			sse_values[j],
-			', '.join(['{:.3}'.format(attr_val) for attr_val in centroids[j]]))
+			', '.join(['{:.5}'.format(attr_val) for attr_val in centroids[j]]))
 
-	print "total sse: %f" % sum(sse_values)
+	total_sse = sum(sse_values)
 
+	print "total sse: %f" % total_sse
 	print "\n"
+
+
+iris_data, iris_keys = data.iris()
+
+stats = summary_stats(iris_data, iris_keys)
+
+#for euclidean
+optimal_clustering = 0
+minimal_sse = 9999
+clusterings = []
+
+for t in range(20):
+
+	i = 3
+
+	print "iris statistics:"
+	print stats
+
+	print "kmeans result clusters based on euclidean distance for k = %d (trial %d):" % (i, t)
+
+	clusters, centroids = kmeans(iris_data, k=i, distance=distance.euclidean,
+							prototype=prototype.avg)
+
+	print_clustering(iris_data, clusters, centroids)
+
+	current_sse = total_sse(iris_data, clusters, centroids)
+	if(current_sse < minimal_sse):
+		optimal_clustering = t
+		minimal_sse = current_sse
+
+	clusterings.append((clusters, centroids))
+
+
+print "optimal clustering:"
+print_clustering(iris_data, clusterings[optimal_clustering][0], clusterings[optimal_clustering][1])
